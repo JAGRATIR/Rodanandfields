a02d6d6cee0c391533a354651958842234276348
Akshit Rana <akshitrana21@gmail.com>
2022-02-23 18:27:41 +0530

    updated home page

--- Script/index.js
+++ Script/index.js
@@ -26,3 +26,37 @@ bestArr.map(function(elem){
 })
 
 
+// scroll to top button 
+var mybutton = document.querySelector(".scroll-up-btn");
+mybutton.addEventListener("click",topFunction);
+// When the user scrolls down 40px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+    mybutton.classList.add("show");
+  } else {
+    mybutton.classList.remove("show");
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.documentElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+}
+
+// sticky navbar 
+var lastScrollTop = 0;
+var navbar = document.querySelector("nav");
+window.addEventListener("scroll",function(){
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if(scrollTop>lastScrollTop){
+        navbar.style.top="-140px";
+    }else{
+        navbar.style.top="0";
+    }
+    lastScrollTop=scrollTop;
+})
