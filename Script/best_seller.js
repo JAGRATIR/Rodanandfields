7c6d1f49160bd19cc215f7f2ca143b33b37f56ab
Akshit Rana <akshitrana21@gmail.com>
2022-02-23 14:29:28 +0530

    Added All products and best seller section

---
+++ Script/best_seller.js
@@ -0,0 +1,134 @@
+var bestArr=
+[
+    {
+        img: "https://www.rodanandfields.com/en-us/medias/ENHLSH01-Regimen-Explainer-R-F-Lash-Boost-570x570-Desktop.png?context=bWFzdGVyfGltYWdlc3wxMDc0OHxpbWFnZS9wbmd8aW1hZ2VzL2hmMS9oYWMvMTQ1MzIwNDI1ODgxOTAucG5nfDQyNTQ0OGUzYmMwZTJkM2YzNzE1MjE2NzQ1M2YxMWQ5M2E3ZWQ2MjAwMDA3MzFmNjQyOWUwNDU3OWZmMTkzZDY",
+        title: "Best Seller",
+        name: "R + F Lash Boost",
+        disc: "Applied nightly to promote the appearance of longer, stronger and darker-looking lashes.",
+        price: 155,
+        dprice: "",
+        qty: 1,
+        star: 4.6,
+        review: "(6808)",
+        pearks: false,
+        pvalue: 155
+    },
+
+    {
+        img: "https://www.rodanandfields.com/en-us/medias/TRFS-Regimen-Explainer-GLOBAL-570x570.png?context=bWFzdGVyfGltYWdlc3wyODM2M3xpbWFnZS9wbmd8aW1hZ2VzL2g5Yy9oNzcvMTM3NTgzODUzODk1OTgucG5nfGYyZGUwNzdmOWM3N2Q2M2FlMjM4Zjc4MjQxNWZkNmIzOTc0NjIyZWM3MDgyZTY3YzA5YjMyZjNhZWY3MGE3YzY",
+        title: "Best Seller",
+        name: "Total RF Serum",
+        disc: "Energizes millions of surface skin cells by 200% to address all key signs of skin aging.",
+        price: 175,
+        dprice: "",
+        qty: 1,
+        star: 4.5,
+        review: "(2466)",
+        pearks: false,
+        pvalue: 175
+    },
+
+    {
+        img: "https://www.rodanandfields.com/en-us/medias/MFEC-Regimen-Explainer-GLOBAL-570x570.png?context=bWFzdGVyfGltYWdlc3wyMzc3NHxpbWFnZS9wbmd8aW1hZ2VzL2gzNS9oZmMvMTM3NTgzODU0NTUxMzQucG5nfDNmYWYyNzdiZGY5OWIyY2UzMDYyZDliYmM2YmUzMTRmOGY3Yzg0YmY1ZjY2YjgyYWU1OGNhZDZiN2IzMjViZGI",
+        title: "Best Seller",
+        name: "REDEFINE Multi-Function Eye Cream",
+        disc: "Visibly firms + lifts to improve the look of fine lines, wrinkles, sagging + crow’s feet.",
+        price: 70,
+        dprice: "",
+        qty: 1,
+        star: 4.6,
+        review: "(8348)",
+        pearks: false,
+        pvalue: 70
+    },
+
+    {
+        img: "https://www.rodanandfields.com/en-us/medias/AAWA125-AATN125-AATT030-AAPM030-REDEFINE-Regimen-Explainer-570x570.png?context=bWFzdGVyfGltYWdlc3wzMTE2NXxpbWFnZS9wbmd8aW1hZ2VzL2hkNy9oMWMvMTM3NTgzODQwNzg4NzgucG5nfDE5NTM4Yzc1OTA0MWNiY2M5NzYwMDZhMTQ0NGVmZDEzZmRhYzVlZWE4YWRhODk3NWQ0ZGU1NDlhNzVjZTUyOTU",
+        title: "Best Seller",
+        name: "REDEFINE Regimen",
+        disc: "Visibly lifts, sculpts + firms while minimizing the appearance of lines + deep wrinkles.",
+        price: 220,
+        dprice: "($275 Value)",
+        qty: 1,
+        star: 4.7,
+        review: "(6405)",
+        pearks: true,
+        pvalue: 197
+    },
+
+    {
+        img: "https://www.rodanandfields.com/en-us/medias/RVWA125-RVTG125-RVSS050-RVTTG50-REVERSE-Regimen-Explainer-570x570.png?context=bWFzdGVyfGltYWdlc3w0MTY5NHxpbWFnZS9wbmd8aW1hZ2VzL2gyNy9oZWIvMTM3NTgzODQzNDEwMjIucG5nfGI3NmIzMjI0NTc2Y2RiZjZlNTdhYjg0ZDM4ZjM0MmM2M2U5Yjg5Y2RiNTM1NTM1NGFlOWNiY2E5ZTVkYTM5NWY",
+        title: "Best Seller",
+        name: "REVERSE Regimen",
+        disc: "Brightens + refines skin’s surface while tackling the look of fine lines + dark spots.",
+        price: 195,
+        dprice: "($245 Value)",
+        qty: 1,
+        star: 3.8,
+        review: "(1365)",
+        pearks: true,
+        pvalue: 175
+    },
+
+    {
+        img: "https://www.rodanandfields.com/en-us/medias/UNWA125-UNTJ125-UNTT030-UNSS030-UNBLEMISH-Explainer-570x570.png?context=bWFzdGVyfGltYWdlc3w1NDk5N3xpbWFnZS9wbmd8aW1hZ2VzL2g2YS9oOTYvMTM3NTgzODQ4NjUzMTAucG5nfDNhOWMxMGU5MjIzMGEyZjNhNTZjNWVkMjUxYmJjODI5YmNlODhjODBkNzJjZTA4ZDk1YTk0Yzk0ZjkyNzdhZmU",
+        title: "Best Seller",
+        name: "UNBLEMISH Regimen",
+        disc: "Helps clear and prevent breakouts while visibly minimizing pores and fine lines.",
+        price: 190,
+        dprice: "($240 Value)",
+        qty: 1,
+        star: 4.5,
+        review: "(906)",
+        pearks: true,
+        pvalue: 171
+    },
+
+  
+
+    {
+        img: "https://www.rodanandfields.com/en-us/medias/ENHEFG1-570x570-Desktop.png?context=bWFzdGVyfGltYWdlc3wyMjI3N3xpbWFnZS9wbmd8aW1hZ2VzL2hkMS9oM2MvMTQ1MzIxNjY2MTUwNzAucG5nfGZlMDQ0NDRlMjczNDFlZjdkYWVmNjkwZWY4MDAwM2I4MjMwZGZiMzU3ZTViZGNlYjllNzBiZjAwY2FlZDU5NzA",
+        title: "Best Seller",
+        name: "Active Hydration Serum",
+        disc: "Instantly boosts hydration levels by 200% and locks in moisture on skin's surface.",
+        price: 112,
+        dprice: "",
+        qty: 1,
+        star: 4.8,
+        review: "(7660)",
+        pearks: false,
+        pvalue: 112
+    },
+
+    {
+        img: "https://www.rodanandfields.com/en-us/medias/ENPS125-570x570-Desktop.png?context=bWFzdGVyfGltYWdlc3wxODU1NXxpbWFnZS9wbmd8aW1hZ2VzL2gyNi9oZjgvMTQ1MzIxNjc2MzA4NzgucG5nfGM0YTc0NjM0YWIzM2QxZDZlOTc0NTdmYjA4M2EyNDA5NGJhODg3ZDQwMGJjZjdiMTZlYTI2MjYwNjI2Y2JhZGE",
+        title: "Best Seller",
+        name: "Rodan + Fields Enhancements",
+        disc: "Exfoliates away dead skin cells for smoother, vibrant-looking skin after just one use. ",
+        price: 83,
+        dprice: "",
+        qty: 1,
+        star: 4.8,
+        review: "(3755)",
+        pearks: false,
+        pvalue: 83
+    },
+
+    {
+        img: "https://www.rodanandfields.com/en-us/medias/MFEC-Regimen-Explainer-GLOBAL-570x570.png?context=bWFzdGVyfGltYWdlc3wyMzc3NHxpbWFnZS9wbmd8aW1hZ2VzL2gzNS9oZmMvMTM3NTgzODU0NTUxMzQucG5nfDNmYWYyNzdiZGY5OWIyY2UzMDYyZDliYmM2YmUzMTRmOGY3Yzg0YmY1ZjY2YjgyYWU1OGNhZDZiN2IzMjViZGI",
+
+        title: "Best Seller",
+        name: "Active Hydration Body Replenish",
+        disc: "Instantly and continuously hydrates the body for younger-looking, younger-acting skin.",
+        price: 66,
+        dprice: "",
+        qty: 1,
+        star: 4.9,
+        review: "(6346)",
+        pearks: false,
+        pvalue: 66
+    },
+
+   
+
+]
